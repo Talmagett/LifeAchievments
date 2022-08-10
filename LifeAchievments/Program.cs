54fd44ad15bb295501d66ee9295482baf5402388
Talgarbek Tilekmatov <talmagett@gmail.com>
2022-08-10 22:30:41 +0600

    Finished, Alhamdulillah

--- LifeAchievments/Program.cs
+++ LifeAchievments/Program.cs
@@ -21,8 +21,8 @@ System.Diagnostics.Debug.WriteLine(Extenstions.GetHerokuConnectionString());
 // Add services to the container.sss
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseSqlServer(connectionString);
-    //options.UseNpgsql(connectionString);
+    //options.UseSqlServer(connectionString);
+    options.UseNpgsql(connectionString);
 });
 builder.Services.AddRazorPages(options =>
 {
@@ -33,7 +33,11 @@ builder.Services.AddRazorPages();
 builder.Services.AddAntiforgery(o => o.HeaderName = "XSRF-TOKEN");
 
 var app = builder.Build();
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else if (app.Environment.IsStaging())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
